0909b3c01481f8825ef194077a4caed6e048c73c
Blodgett, David L <dblodgett@usgs.gov>
2014-02-12 12:57:46 -0600

    Missed a file save.

--- R/calculateStatsGroups.R
+++ R/calculateStatsGroups.R
@@ -13,7 +13,22 @@ calculateStatsGroups<-function(stats, sites, startdate, enddate, X_DATA_FUN, x_a
       # In the case th drainage area function input is single valued (a list of urls)
       drain_area<-DRAIN_AREA_FUN(drain_args[1])
     }
-    tempArrays<-runStatsGroups(x_data,tempArrays,i,drain_area)
+    if (nrow(x_data) > 2) {
+      flow_data <- get_obsdata(x_data)
+      tempArrays$min_date[i] <- as.character(min(flow_data$date))
+      tempArrays$max_date[i] <- as.character(max(flow_data$date))
+      flow_data <- flow_data[, c("wy_val", "date", "discharge", "month_val", "year_val", "day_val", "jul_val")]
+      if (ncol(tempArrays$ObsFlowStats) > 0) {
+        tempArrays$ObsFlowStats[i, ] <- FlowStatsAll(flow_data, drain_area, stats=stats)
+      }
+      if (ncol(tempArrays$magnifSevenObs) > 0) {
+        tempArrays$magnifSevenObs[i, ] <- magnifSeven(flow_data)
+      }
+      tempArrays$comment <- ""
+    } else {
+      tempArrays$comment[i] <- "No observed data for this site"
+    }
+    #tempArrays<-runStatsGroups(x_data,tempArrays,i,drain_area)
   }
   statsout<-nameStatsArray(stats, sites, tempArrays)
   return(statsout)
