34614e5ab6f8992a9f746b3a23eaf1a42d39cd53
Jessica Thompson <jlthomps@usgs.gov>
2014-05-14 09:19:04 -0500

    new build and vignette

--- R/includedData.R
+++ R/includedData.R
@@ -4,19 +4,35 @@
 #' @docType data
 #' @author Jessica Thompson \email{jlthomps@@usgs.gov}
 #' @keywords data
+NULL
+#' This is data to be included in my package
+#'
 #' @name mod_data
 #' @docType data
 #' @author Jessica Thompson \email{jlthomps@@usgs.gov}
 #' @keywords data
+NULL
+
+#' Example data
+#'
+#' Example data 
+#'
+#' @name sampleData
+#' @docType data
+#' @keywords streamflow data
+NULL
+
+#' This is data to be included in my package
+#'
 #' @name qfiletempf
 #' @docType data
 #' @author Jessica Thompson \email{jlthomps@@usgs.gov}
 #' @keywords data
+NULL
+
+#' This is data to be included in my package
+#'
 #' @name dailyData
-#' @docType data
-#' @author Jessica Thompson \email{jlthomps@@usgs.gov}
-#' @keywords data
-#' @name sampleData
 #' @docType data
 #' @author Jessica Thompson \email{jlthomps@@usgs.gov}
 #' @keywords data
