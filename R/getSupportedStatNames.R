002dbc51b8819a6994312739e2b3eb2f32eb8b2f
David Blodgett <dblodgett@usgs.gov>
2016-12-22 16:50:03 -0600

    Missing some stats names that are in EflowStats now.

--- R/getSupportedStatNames.R
+++ R/getSupportedStatNames.R
@@ -8,9 +8,9 @@
 getSupportedStatNames <- function() {
   namesMagnif <- c("lam1Obs", "tau2Obs", "tau3Obs", "tau4Obs", "ar1Obs", "amplitudeObs", "phaseObs")
   namesMagStat <- c(paste('ma',sequence(45),sep=""), paste('ml',sequence(22),sep=""),paste('mh',sequence(27),sep=""))
-  namesFlowStat <- c(paste('fl',sequence(3),sep=""),paste('fh',sequence(10),sep=""))
-  namesDurStat <- c(paste('dl',sequence(20),sep=""),paste('dh',sequence(21),sep=""))
-  namesTimStat <- c(paste('ta',sequence(2),sep=""),paste('tl',sequence(2),sep=""),paste('th',sequence(2),sep=""))
+  namesFlowStat <- c(paste('fl',sequence(3),sep=""),paste('fh',sequence(11),sep=""))
+  namesDurStat <- c(paste('dl',sequence(20),sep=""),paste('dh',sequence(24),sep=""))
+  namesTimStat <- c(paste('ta',sequence(3),sep=""),paste('tl',sequence(4),sep=""),paste('th',sequence(3),sep=""))
   namesRateStat <- c(paste('ra',sequence(9),sep=""))
   namesOtherStatICP <- c("NWCportalL7Q10Obs", "NWCportalL7Q2Obs", "NWCportalReturn10Obs")
   namesOtherStat <- c("med_flowObs", "cv_flowObs", "cv_dailyObs", "flow_10Obs", "flow_25Obs", "flow_50Obs", "flow_75Obs", "flow_90Obs", "flow_15Obs")
