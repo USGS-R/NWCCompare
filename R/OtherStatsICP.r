6554b94e37123474602d3ab2ff64251b51dacb3a
David Blodgett <dblodgett@usgs.gov>
2017-04-15 21:10:28 -0500

    Getting NWCPortal functions into OtherStats.

--- R/OtherStatsICP.r
+++ R/OtherStatsICP.r
@@ -46,3 +46,80 @@ OtherStatsICP<-function(data)  {
   OtherStatsv <- c(med_flow,cv_flow,l7Q10v,l7Q2v,return_10v,flow_10,flow_25,flow_50,flow_75,flow_90,flow_15)
   return(OtherStatsv)
 }
+
+#' Function to return the l7Q10 value for a given data series
+#' 
+#' This function accepts a data frame containing daily data and returns the l7Q10 value, 
+#' calculated for the National Water Census by a non-standard method.
+#' 
+#' @param qfiletempf data frame containing value data for one of the chosen timeseries
+#' @return NWCportalL7Q10 l7Q10 value for the given data frame
+#' @importFrom stats aggregate
+#' @examples
+#' qfiletempf<-sampleData
+#' NWCportalL7Q10(qfiletempf)
+NWCportalL7Q10 <- function(qfiletempf) {
+  day7mean <- rollmean(qfiletempf$discharge, 7, align = "right", 
+                       na.pad = TRUE)
+  day7rollingavg <- data.frame(qfiletempf, day7mean)
+  rollingavgs7day <- subset(day7rollingavg, day7rollingavg$day7mean != 
+                              "NA")
+  min7daybyyear <- aggregate(rollingavgs7day$day7mean, 
+                             list(rollingavgs7day$wy_val), min, na.rm=TRUE)
+  sort_7day<-sort(min7daybyyear$x)
+  rank_90<-floor(findrank(length(sort_7day),0.90))
+  if (rank_90 > 0) { 
+    l7Q10<-round(sort_7day[rank_90],digits=2)
+  } else { 
+    l7Q10<-NaN 
+  }
+  return(l7Q10)
+}
+
+#' Function to return the l7Q2 value for a given data series
+#' 
+#' This function accepts a data frame containing daily data and returns the l7Q2 value, 
+#' calculated for the National Water Census by a non-standard method.
+#' 
+#' @param qfiletempf data frame containing value data for one of the chosen timeseries
+#' @return NWCportalL7Q2 l7Q2 value for the given data frame
+#' @importFrom stats aggregate
+#' @examples
+#' qfiletempf<-sampleData
+#' NWCportalL7Q2(qfiletempf)
+NWCportalL7Q2 <- function(qfiletempf) {
+  day7mean <- rollmean(qfiletempf$discharge, 7, align = "right", 
+                       na.pad = TRUE)
+  day7rollingavg <- data.frame(qfiletempf, day7mean)
+  rollingavgs7day <- subset(day7rollingavg, day7rollingavg$day7mean != 
+                              "NA")
+  min7daybyyear <- aggregate(rollingavgs7day$day7mean, 
+                             list(rollingavgs7day$wy_val), min, na.rm=TRUE)
+  sort_7day<-sort(min7daybyyear$x)
+  rank_50<-floor(findrank(length(sort_7day),0.50))
+  if (rank_50 > 0) { 
+    l7Q2<-round(sort_7day[rank_50],digits=2) 
+  } else { 
+    l7Q2<-NaN 
+  }
+  return(l7Q2)
+}
+
+#' Function to return the 10 year return value for a given data series
+#' 
+#' This function accepts a data frame containing daily data and returns the 10 year return value, 
+#' calculated for the National Water Census by a non-standard method.
+#' 
+#' @param qfiletempf data frame containing value data for the chosen timeseries
+#' @return NWCportalReturn10 10-year return value for the given data frame
+#' @importFrom stats aggregate
+#' @examples
+#' qfiletempf<-sampleData
+#' NWCportalReturn10(qfiletempf)
+NWCportalReturn10 <- function(qfiletempf) {
+  annual_max <- aggregate(qfiletempf$discharge, list(qfiletempf$wy_val), max, na.rm=TRUE)
+  sort_annual_max <- sort(annual_max$x)
+  rank_10 <- floor(findrank(length(sort_annual_max),0.10))
+  return_10 <- sort_annual_max[rank_10]
+  return(return_10)
+}
