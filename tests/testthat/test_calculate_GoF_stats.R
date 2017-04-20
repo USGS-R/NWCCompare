3ca5adf39d59afd56859870a3abb27646e8e9322
David Blodgett <dblodgett@usgs.gov>
2017-04-20 15:05:30 -0500

    characterization test specific to GoF stats

---
+++ tests/testthat/test_calculate_GoF_stats.R
@@ -0,0 +1,14 @@
+context("calculate_GoF_stats")
+library(EflowStats)
+
+test_that("test example returns the same", {
+Gaged <- obs_data
+Gaged$date <- as.Date(Gaged$date)
+Gaged <- dataCheck(Gaged, yearType = "water")
+Modeled<-mod_data
+Modeled$date <- as.Date(Modeled$date)
+Modeled <- dataCheck(Modeled, yearType = "water")
+GoFstats <- calculate_GoF_stats(Modeled,Gaged)
+Gofstats_check <- readRDS("data/test_calculate_GoF_stats.rds")
+expect_equal(GoFstats, Gofstats_check)
+})
