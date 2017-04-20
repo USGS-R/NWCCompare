d2ed16761ee832aee4609adc359403caa4059e45
David Blodgett <dblodgett@usgs.gov>
2017-04-20 14:30:49 -0500

    removed get_obsdata. Updated get_local_data.

--- R/get_local_data.R
+++ R/get_local_data.R
@@ -1,26 +1,30 @@
 #' Function to return flow data from a locally stored file
 #' 
-#' This function accepts a list of sites, start and end date and returns a data frame containing requested flow data
+#' This function accepts a list of sites, start and end years 
+#' and returns a data frame containing requested flow data
 #' 
-#' @param dataPath path to directory containing data files
-#' @param startDt beginning water year, will be translated to 10/01
-#' @param endDt ending water year, will be translated to 09/30
-#' @param sepChar string containing the datafile separator, default is comma
+#' @param data_path path to directory containing data files
+#' @param start_year beginning water year, will be translated to 10/01
+#' @param end_year ending water year, will be translated to 09/30
+#' @param sep_char string containing the datafile separator, default is comma
 #' @return dataOut data frame containing requested flow data for the stations
 #' @export
 #' @importFrom readr read_delim
 #' @examples
-#' dataPath <- system.file("extdata", package="NWCCompare")
-#' dataPath <- paste(dataPath, "modeled", sep="/")
-#' startdate <- "2009"
-#' enddate <- "2013"
-#' localData <- get_local_data(dataPath,startDt=startdate,endDt=enddate)
-get_local_data <- function(dataPath, startDt="", endDt="", sepChar=",") {
-  if (nchar(startDt)>1) {startdate <- paste(startDt,"10","01",sep="-")}
+#' data_path <- system.file("extdata", package="NWCCompare")
+#' data_path <- paste(data_path, "modeled", sep="/")
+#' start_year <- "2007"
+#' end_year <- "2010"
+#' localData <- get_local_data(data_path,start_year=start_year,end_year=end_year)
+get_local_data <- function(data_path, start_year="", end_year="", sep_char=",") {
   
-  if (nchar(endDt)>1) {enddate <- paste(endDt,"09","30",sep="-")}
+  yearType <- "water" # could implement both calendar types in the future.
   
-  fileList <- list.files(path=dataPath)
+  if (nchar(start_year)>1) {start_date <- paste(start_year,"10","01",sep="-")}
+  
+  if (nchar(end_year)>1) {end_date <- paste(end_year,"09","30",sep="-")}
+  
+  fileList <- list.files(path=data_path)
   
   for (i in 1:length(fileList)) {
     fileList[i] <- ifelse(nchar(strsplit(fileList[i], ".csv")) < nchar(fileList[i]) | 
@@ -46,47 +50,37 @@ get_local_data <- function(dataPath, startDt="", endDt="", sepChar=",") {
   dataOut <- list()
   
   for (i in 1:length(sites)) {
-    x_obs <- read_delim(file = file.path(dataPath,sites[i]),
-                        delim = sepChar,
+    x_obs <- read_delim(file = file.path(data_path,sites[i]),
+                        delim = sep_char,
                         col_types = "ccd",
                         col_names = TRUE)
     
     site <- unlist(x_obs[1,1])
-    x_obs <- x_obs[,2:3]
+    x_obs <- as.data.frame(x_obs[,2:3], stringsAsFactors = "false")
     
-    if(!dateFormatCheck(unlist(x_obs[,1]))){
+    if(!dateFormatCheck(unlist(x_obs[,1])[1])){
       x_obs[,1] <- as.character(as.Date(x_obs[,1],format="%m/%d/%Y"))
     }
     
-    obs_data <- get_obsdata(x_obs)
-    obs_data$date <- strptime(obs_data$date,format="%Y-%m-%d")
-    if (nchar(startDt)>1) {obs_data<-obs_data[which(strptime(obs_data$date,"%Y-%m-%d")>=strptime(startdate,"%Y-%m-%d")),]}
-    if (nchar(endDt)>1) {obs_data<-obs_data[which(strptime(obs_data$date,"%Y-%m-%d")<=strptime(enddate,"%Y-%m-%d")),]}
-    obs_count<-nrow(obs_data)
-    cat(paste("get_obsdata run on x_obs for site",site,obs_count,"\n"))
-    if (nrow(obs_data)==0) {
-      obs_data<-"No complete water years for site"
-    } else {
-      countbyyr<-aggregate(obs_data$discharge, list(obs_data$wy_val), length)
-      colnames(countbyyr)<-c("wy","num_samples")
-      sub_countbyyr<-countbyyr[countbyyr$num_samples>=365,]
-      if (nrow(sub_countbyyr)==0) {
-        obs_data<-"No complete water years for site"
-      } else {
-        obs_data<-merge(obs_data,sub_countbyyr,by.x="wy_val",by.y="wy")
-        obs_data<-obs_data[order(obs_data$date),]
-        obs_data <- obs_data[,c('wy_val','date','discharge','month_val','year_val','day_val','jul_val')]
-        site_no <- rep(site,nrow(obs_data))
-        obs_data <- cbind(site_no,obs_data,stringsAsFactors=FALSE)
-        }
-      dataOut[[i]]<-obs_data
+    x_obs[,1] <- as.Date(x_obs[,1])
+
+    if (nchar(start_year)>1) {
+      x_obs <- x_obs[which(strptime(x_obs$date,"%Y-%m-%d") >= strptime(start_date,"%Y-%m-%d")),]
     }
+    
+    if (nchar(end_year)>1) {
+      x_obs <- x_obs[which(strptime(x_obs$date, "%Y-%m-%d") <= strptime(end_date, "%Y-%m-%d")),]
+    }
+    
+    obs_data <- dataCheck(x_obs, yearType = "water")
+
+    dataOut[[site]]<-obs_data
   }
   return(dataOut)
 }
 
-dateFormatCheck <- function(date){  # checks for the format YYYY-MM-DD
-  parts <- strsplit(date,"-",fixed=TRUE)
+dateFormatCheck <- function(date_ex){  # checks for the format YYYY-MM-DD
+  parts <- strsplit(date_ex,"-",fixed=TRUE)
   condition <- FALSE
   if (length(parts[[1]])>1) {
     if (nchar(parts[[1]][1]) == 4 && nchar(parts[[1]][2]) == 2 && nchar(parts[[1]][3]) == 2){
--- tests/testthat/test_get_local_data.R
+++ tests/testthat/test_get_local_data.R
@@ -5,7 +5,7 @@ dataPath <- system.file("extdata", package="NWCCompare")
 dataPath <- paste(dataPath, "modeled", sep="/")
 startdate <- "2009"
 enddate <- "2013"
-localData <- get_local_data(dataPath,startDt=startdate,endDt=enddate)
+localData <- get_local_data(dataPath, startdate, enddate)
 localData_check <- readRDS("data/test_get_local_data.rds")
 expect_equal(localData, localData_check)
 })
